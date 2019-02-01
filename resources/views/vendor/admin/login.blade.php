1cba1f97eb48c94e352f58162627c3611360058f
nitish.garg1512 <nitish.garg1512@gmail.com>
2019-02-02 00:45:07 +0700

    sign in

--- resources/views/vendor/admin/login.blade.php
+++ resources/views/vendor/admin/login.blade.php
@@ -27,10 +27,13 @@
 <body class="hold-transition login-page" @if(config('admin.login_background_image'))style="background: url({{config('admin.login_background_image')}}) no-repeat;background-size: cover;"@endif>
 <div class="powerby">
     Powered by BallerProfile
-    
 </div>
 <div class="login-box">
+  <div class="login-avatar">
+       <img src="/img/logo_league.png" />
+  </div>
   <div class="login-logo">
+
     <a href="{{ admin_base_path('/') }}"><b>{{config('admin.name')}}</b></a>
   </div>
   <!-- /.login-logo -->
