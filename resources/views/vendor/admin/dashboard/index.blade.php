096eef913f3fff49e38b27f0bb0f929dc1fd798f
nitish.garg1512 <nitish.garg1512@gmail.com>
2019-02-08 12:38:11 +0700

    dashboard

--- resources/views/vendor/admin/dashboard/index.blade.php
+++ resources/views/vendor/admin/dashboard/index.blade.php
@@ -4,7 +4,28 @@
     <div class="col-md-12">
         <div class="box box-solid box-default">
             <div class="box-body">
-                Venue with Biggest Following
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-4 f20 d-venue">35 Venues</div>
+                            <div class="col-md-4 f20 d-gopw">2404 GOPW</div>
+                            <div class="col-md-4 f20 d-hplus">100 H+</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4 f20 d-leagues">116 Leagues</div>
+                            <div class="col-md-4 f20 d-teams">928 Teams</div>
+                            <div class="col-md-4 f20 d-tplus">230 T+</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4 f20 d-region">14 Regions</div>
+                            <div class="col-md-4 f20"></div>
+                            <div class="col-md-4 f20 d-fplus">135 F+v</div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <button type="button" class="btn btn-primary btn-main-primary shadow pull-right">Add Venue +</button>
+                    </div>
+                </div>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
     </div>
@@ -14,21 +35,123 @@
     <div class="col-md-4">
         <div class="box box-solid box-default">
             <div class="box-body">
-                Attendance
+                <div class="d-title">Attendance</div>
+                <table class="table table-condensed">
+                    <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>4.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>5.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
     </div>
     <div class="col-md-4">
         <div class="box box-solid box-default">
             <div class="box-body">
-                GOPW
+                <div class="d-title">GOPW</div>
+                <table class="table table-condensed">
+                    <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>4.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>5.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
     </div>
     <div class="col-md-4">
         <div class="box box-solid box-default">
             <div class="box-body">
-                New teams joined
+                <div class="d-title">New teams joined</div>
+                <table class="table table-condensed">
+                    <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>4.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    <tr>
+                        <td>5.</td>
+                        <td>Tottenham</td>
+                        <td>55%</td>
+                        <td>123</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
     </div>
